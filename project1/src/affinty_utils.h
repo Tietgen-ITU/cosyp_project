191bc7985eb6188e4ca6f890697004174fd8ebd3
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-02 09:11:07 +0100

    Fix import of affinity_utils

--- project1/src/affinty_utils.h
+++ project1/src/affinty_utils.h
@@ -1,3 +1,6 @@
+#ifndef CORE_AFFINITY
+#define CORE_AFFINITY
+
 #ifdef WITH_CORE_AFFINITY
 
 // Add core affinity dependency
@@ -10,4 +13,6 @@ Gets the thread id by the given index.
 It prioritizes getting thread ids that is on the same core.
 */
 #define get_thread_id(i) ((should_get_neighbour_thread(i)) ? (15 + (i * 2)) : (i * 2))
+
+#endif
 #endif
