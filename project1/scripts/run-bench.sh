dd6a87026366e28e844de6d092f6cd7fcb2523a3
Adrian Borup <adrian@borup.com>
2024-03-13 15:28:35 +0100

    Update benchmark script to use perf

--- project1/scripts/run-bench.sh
+++ project1/scripts/run-bench.sh
@@ -10,31 +10,36 @@ tuples=(16777216)
 algorithms=("independent" "concurrent")
 binaries=("project1" "project1-ca")
 
-mkdir -p bench-out
-outfile="bench-out/bench-$timestamp.txt"
+perf_events="context-switches,cpu-migrations,dTLB-load-misses,L1-dcache-load-misses,LLC-load-misses"
 
-echo -e "repetition \t threads \t hash bits \t tuples \t binary \t algorithm \t time" | tee -a $outfile
+outdir="bench-out/bench-$timestamp"
+mkdir -p $outdir
 
 for repetition in {1..8}
 do :
   for t in "${threads[@]}"
-  do : 
+  do :
     for h in "${hash_bits[@]}"
-    do : 
+    do :
       for n in "${tuples[@]}"
-      do : 
+      do :
         for a in "${algorithms[@]}"
-        do : 
+        do :
           for binary in "${binaries[@]}"
-          do : 
-            elapsed=$(./$binary -t $t -h $h -n $n -a $a | awk '/Elapsed: /{print $2 " " $3}')
-            echo -e "$repetition \t $t \t $h \t $n \t \t $binary \t $a \t $elapsed" | tee -a $outfile
+          do :
+            run_command="./$binary -t $t -h $h -n $n -a $a"
+
+            now=$(date +"%Y-%m-%d %H:%M:%S")
+            echo -e "[$now] benching $run_command"
+
+            outfile="bench-$a-t$t-h$h-n$n-$binary-r$repetition.txt"
+            perf stat -e $perf_events $run_command &> "$outdir/$outfile"
+
+            now=$(date +"%Y-%m-%d %H:%M:%S")
+            echo -e "[$now] finished (wrote to $outfile)"
           done
         done
       done
     done
   done
-
-  echo -ne "\n"
 done
-
