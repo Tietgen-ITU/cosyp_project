4bd8632e4202fe221404f62de5e912b6a3b57b7f
Adrian Borup <adrian@borup.com>
2024-04-17 13:11:44 +0200

    Insert data into elasticsearch

--- project2/load-data/populate_data.py
+++ project2/load-data/populate_data.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 import psycopg2
 from psycopg2.extras import execute_batch
 from typing import Union
+from elasticsearch import Elasticsearch
 
 
 DUMP_HOST_URL = "https://dumps.wikimedia.org"
@@ -80,10 +81,11 @@ def insert_into_postgres(pages: list[Union[str, str]]):
     con = psycopg2.connect("postgresql://cosyp-sa:123@localhost:5049/cosyp")
     cur = con.cursor()
 
-    print("Inserting pages")
-    execute_batch(cur, "INSERT INTO articles (title, body) VALUES (%s, %s)", pages)
+    print("Inserting pages into Postgres")
+    execute_batch(
+        cur, "INSERT INTO articles (title, body) VALUES (%s, %s)", pages)
     con.commit()
-    print("Inserted pages")
+    print("Inserted pages into Postgres")
 
     # To create a full text search index:
     #
@@ -99,12 +101,32 @@ def insert_into_postgres(pages: list[Union[str, str]]):
     #   ORDER BY rank DESC;
 
 
+def insert_into_elasticsearch(pages: list[Union[str, str]]):
+    es = Elasticsearch("http://localhost:9200")
+
+    print("Indexing documents in Elasticsearch")
+
+    for i, page in enumerate(pages, start=1):
+        title, body = page
+        doc = {"title": title, "body": body}
+        es.index(index="articles", id=title, document=doc)
+
+        if i % 100 == 0 or i == 1:
+            print(f"\rIndexed {i} documents", end="")
+
+    print("\rIndexed documents in Elasticsearch")
+
+    # Query:
+    #   es.search(index="articles", query={'match':{'body':'serach term'}}, fields=["title"], source=False)
+
+
 def main():
     # get_all_files()
     # decompress_articles()
     pages = load_articles_xml(
         "articles/decompressed/enwiki-20240401-pages-articles1.xml-p1p41242")
-    insert_into_postgres(pages)
+    # insert_into_postgres(pages)
+    # insert_into_elasticsearch(pages)
     pass
 
 
