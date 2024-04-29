b3badae67d791d986f6ad88a43839194a0f398ba
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-04-29 19:49:19 +0200

    Add parameters to sched-job

--- project2/sched-job.py
+++ project2/sched-job.py
@@ -1,25 +1,34 @@
+import sys
 import paramiko
 import getpass
-import time
-import os
 from scp import SCPClient
 
-username = input('Enter your username: ')
-password = getpass.getpass('Enter your password: ')
+def copy_to_remote(ssh, local_dir, remote_dir):
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(local_dir, remote_dir, recursive=True)
 
+if __name__ == "__main__":
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('hpc.itu.dk', username=username, password=password)
+    if len(sys.argv) < 2:
+        print('Usage: python sched-job.py <psql | elastic>')
+        sys.exit(1)
 
-# Upload files from our src directory to the remote directory
-local_dir = './src'
-remote_dir = '~/cosyp'
-with SCPClient(ssh.get_transport()) as scp:
-    scp.put(local_dir, remote_dir, recursive=True)
+    dbms_type = sys.argv[1]
 
-# Schedule jobs
-remote_job_file = '~/cosyp/psql.job' # TODO: This should maybe be a parameter or an argument to the script
-stdin, stdout, stderr = ssh.exec_command(f'sbatch {remote_job_file}')
-job_id = int(stdout.read().decode().split()[-1])
-print(f'Submitted job with ID {job_id}')
+    username = input('Enter your username: ')
+    password = getpass.getpass('Enter your password: ')
+
+    print('Connecting to HPC...')
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('hpc.itu.dk', username=username, password=password)
+
+    # Upload files from our src directory to the remote directory
+    print('Copying files to remote...')
+    copy_to_remote(ssh, './src', '~/cosyp')
+
+    # Schedule jobs
+    remote_job_file = f'~/cosyp/{dbms_type}.job'
+    stdin, stdout, stderr = ssh.exec_command(f'sbatch {remote_job_file}')
+    job_id = int(stdout.read().decode().split()[-1])
+    print(f'Submitted job with ID {job_id}')
