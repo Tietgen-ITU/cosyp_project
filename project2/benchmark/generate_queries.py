4c85f7f57d4408738173db0f3429becc6dfdd0e6
Adrian Borup <adrian@borup.com>
2024-05-13 10:40:36 +0200

    Move query exploration to actual guery generation

--- project2/benchmark/generate_queries.py
+++ project2/benchmark/generate_queries.py
@@ -1,20 +1,74 @@
+from collections import defaultdict
 import string
 import random
 from itertools import cycle
+import json
+import sys
+import psycopg2
+import os
+
+
+STOP_WORDS = set("a, an, and, are, as, at, be, but, by, for, if, in, into, is, it, no, not, of, on, or, such, that, the, their, then, there, these, they, this, to, was, will, with".split(", "))
+
+
+def load_word_freqs(path):
+    # print("Loading word frequencies from", path)
+    with open(path, "r") as f:
+        word_freqs = json.load(f)
+    # print("Loaded word frequencies")
+
+    # print("Filtering words")
+    banned = set(STOP_WORDS)
+    for bw in banned:
+        word_freqs.pop(bw, None)
+    # print("Filtered words")
+
+    # items = list(word_freqs.items())
+    # print(f"Total number of words {len(items):,}")
+    # print(f"Total number of occurrences: {sum(item[1] for item in items):,}")
+    # percentiles = [0.0001, 0.001, 0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9]
+    # for p in percentiles:
+    #     index = int(len(items) * p)
+    #     print(f"{p * 100}-percentile:",
+    #           items[index], f"({index:,} words appear at least {items[index][1]:,} times)")
+
+    return word_freqs
+
+
+word_article_freqs = None
+cache_file = "search_terms_cache.json"
 
 
 def generate_search_terms(cur, num_queries=10, max_articles_sourced=1000, num_words=(1, 6), seed=None):
+    if not os.path.exists(cache_file):
+        with open(cache_file, "w") as f:
+            json.dump({}, f)
+    with open(cache_file, "r") as f:
+        current_cache = json.load(f)
+    cache_key = f"{num_queries}_{max_articles_sourced}_{num_words[0]}-{num_words[1]}_{seed}"
+
+    if cache_key in current_cache:
+        print("Using cached search terms for key", cache_key)
+        return current_cache[cache_key]
+
+    global word_article_freqs
+    if word_article_freqs is None:
+        word_article_freqs = load_word_freqs("word_article_freqs.json")
+
     random.seed(seed)
 
+    print("Fetching articles")
     random_query = f"""
         SELECT title, body FROM articles
         TABLESAMPLE SYSTEM(100)
         {f"REPEATABLE({seed})" if seed else ""}
-        LIMIT {min(num_queries, max_articles_sourced)};
+        WHERE LENGTH(body) > 100
+        LIMIT {max_articles_sourced};
     """
     cur.execute(random_query)
 
     all = cur.fetchall()
+    print(f"Done fetching {len(all)} articles")
 
     # If we want more search terms than articles fetched, wrap around
     articles_iter = cycle(all)
@@ -25,20 +79,22 @@ def generate_search_terms(cur, num_queries=10, max_articles_sourced=1000, num_wo
     for letter in string.ascii_lowercase + string.ascii_uppercase:
         special_chars.discard(letter)
 
-    banned = set(["REDIRECT", "ref", "nbsp"])
+    banned = set(["REDIRECT", "ref", "nbsp", "s"])
 
-    search_terms = set()
+    search_term_score = {}
+
+    trans = {c: " " for c in special_chars}
+    trans["'"] = ""
+    table = str.maketrans(trans)
 
     min_words, max_words = num_words
 
-    while len(search_terms) < num_queries:
+    while len(search_term_score) < 100 * num_queries:
+        print(f"\rProgress: {len(search_term_score)}/{100 * num_queries}", end="")
         _, body = next(articles_iter)
 
-        trans = {c: " " for c in special_chars}
-        table = str.maketrans(trans)
         normalised = body.translate(table)
-        parts = [x for x in normalised.split() if len(x) >
-                 1 and x not in banned]
+        parts = [x for x in normalised.split() if x not in banned]
 
         length = random.randint(min_words, max_words)
         start_index = random.randint(0, len(parts))
@@ -48,6 +104,123 @@ def generate_search_terms(cur, num_queries=10, max_articles_sourced=1000, num_wo
 
         search_term = " ".join(parts[start_index:start_index + length])
 
-        search_terms.add(search_term.lower())
+        lower_terms = set(
+            term for term in search_term.lower().split() if term not in STOP_WORDS)
+        # with_score = [(term, word_freqs.get(term, 0)) for term in lower_terms]
+        with_article_score = [(term, word_article_freqs.get(term, 0))
+                              for term in lower_terms]
 
-    return list(search_terms)
+        # freq_score = sum(score for _, score in with_score)
+        article_score = sum(score for _, score in with_article_score)
+
+        # print(f"Search term: {search_term}")
+        # print(f"Frequency score: {freq_score}")
+        # print(f"Word frequency scores: {with_score}")
+        # print(f"Article frequency score: {article_score}")
+        # print(f"Word article scores: {with_article_score}")
+
+        search_term_score[search_term.lower()] = (search_term, article_score)
+
+    print(f"\nDone generating search terms")
+
+    sorted_search_terms = sorted(
+        search_term_score.values(), key=lambda x: x[1], reverse=True)
+
+    # for search_term, score in sorted_search_terms:
+    #     print(f"{score}: {search_term}")
+    # print()
+
+    high_cardinality = [term for term, _ in sorted_search_terms[:num_queries]]
+    low_cardinality = [term for term, _ in sorted_search_terms[-num_queries:]]
+    random_queries = [term for term, _ in random.sample(sorted_search_terms, num_queries)]
+
+    results = {
+        'in_many_articles': high_cardinality,
+        'in_few_articles': low_cardinality,
+        'random': random_queries,
+    }
+
+    current_cache[cache_key] = results
+    with open(cache_file, "w") as f:
+        json.dump(current_cache, f, indent=2)
+
+    return results
+
+
+def count_words(cur, seed=None):
+    random.seed(seed)
+
+    print("Fetching articles")
+    random_query = f"""
+        SELECT title, body FROM articles
+        TABLESAMPLE SYSTEM(100)
+        {f"REPEATABLE({seed})" if seed else ""};
+    """
+    cur.execute(random_query)
+
+    all = cur.fetchall()
+    print("Done fetching articles")
+
+    special_chars = set()
+    for _, body in all:
+        special_chars.update(set(body))
+    for letter in string.ascii_lowercase + string.ascii_uppercase:
+        special_chars.discard(letter)
+
+    trans = {c: " " for c in special_chars}
+    trans["'"] = ""
+    table = str.maketrans(trans)
+
+    word_freqs = defaultdict(lambda: 0)
+    word_article_freqs = defaultdict(lambda: 0)
+
+    for i, (_, body) in enumerate(all):
+        if i % 100 == 0:
+            print(f"\rProcessing article {i}/{len(all)}", end="")
+
+        normalised = body.translate(table).lower()
+        parts = [x for x in normalised.split()]
+
+        for part in parts:
+            word_freqs[part.lower()] += 1
+
+        for word in set(parts):
+            word_article_freqs[word] += 1
+
+    print()
+    print("Done processing articles")
+
+    sorted_freqs = sorted(word_freqs.items(), key=lambda x: x[1], reverse=True)
+    sorted_article_freqs = sorted(
+        word_article_freqs.items(), key=lambda x: x[1], reverse=True)
+
+    print("Writing to file")
+
+    with open("word_freqs.json", "w") as f:
+        json.dump(dict(sorted_freqs), f, indent=2)
+    with open("word_article_freqs.json", "w") as f:
+        json.dump(dict(sorted_article_freqs), f, indent=2)
+
+    print("Done writing to file")
+
+
+def connect():
+    con = psycopg2.connect("postgresql://cosyp-sa:123@localhost:5049/cosyp")
+    pg = con.cursor()
+    return pg
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("what should i do?")
+        exit(1)
+
+    pg = connect()
+    if sys.argv[1] == "count":
+        print(count_words(pg, seed=42))
+    elif sys.argv[1] == "gen":
+        print(json.dumps(generate_search_terms(
+            pg, seed=1337, num_words=(4, 4)), indent=2))
+    else:
+        print("idk bro")
+    pg.close()
