ee61d9f79b49dfb0d4f1cddf9d5aa58c032959f1
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-05-14 12:39:28 +0200

    Add finished benchmark script

--- project2/benchmark/run-bench.sh
+++ project2/benchmark/run-bench.sh
@@ -4,19 +4,20 @@ sizes=(1 2 4 8 16 32)
 count=`expr ${#sizes[@]} - 1`
 elastic_port=9200
 psql_port=5049
-# Run the benchmark
 
+export BENCH_FOLDER_NAME="$(date +'%Y-%m-%d_%H-%M-%S')"
+
+# Run the benchmark
 for i in $(seq 0 $count )
 do :
     size=${sizes[i]}
     pport=`expr $psql_port + $i`
     eport=`expr $elastic_port + $i`
     echo "Elastic: ${eport}"
-    # docker compose up "psql-${size}g elasic-${size}g" -d
+    docker compose up "psql-${size}g elasic-${size}g" -d
 
     export COSYP_PSQL_CONNECTION_STRING="postgresql://cosyp-sa:123@localhost:${pport}/cosyp"
     export COSYP_ELASTIC_URL="http://localhost:${eport}"
-    export BENCH_FOLDER_NAME="$(date +'%Y-%m-%d_%H-%M-%S')"
-    # python3 benchmark.py
-    # docker compose down "psql-${size}g elasic-${size}g"
+    python3 benchmark.py
+    docker compose down "psql-${size}g elasic-${size}g"
 done
