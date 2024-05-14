43bff4fe6a473f54cbe8220b7a2d79c21d1df9c0
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-05-14 12:40:35 +0200

    Remove wrong echo

--- project2/benchmark/run-bench.sh
+++ project2/benchmark/run-bench.sh
@@ -13,11 +13,10 @@ do :
     size=${sizes[i]}
     pport=`expr $psql_port + $i`
     eport=`expr $elastic_port + $i`
-    echo "Elastic: ${eport}"
-    docker compose up "psql-${size}g elasic-${size}g" -d
-
     export COSYP_PSQL_CONNECTION_STRING="postgresql://cosyp-sa:123@localhost:${pport}/cosyp"
     export COSYP_ELASTIC_URL="http://localhost:${eport}"
+
+    docker compose up "psql-${size}g elasic-${size}g" -d
     python3 benchmark.py
     docker compose down "psql-${size}g elasic-${size}g"
 done
